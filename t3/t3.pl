41f9b8a7cc3de5b35815eeb6139c6c21b9d1f7a1
TallesSC <37150960+TallesSC@users.noreply.github.com>
2018-04-11 16:07:07 -0300

    Update t3.pl

--- t3/t3.pl
+++ t3/t3.pl
@@ -6,18 +6,15 @@ repete(N, C, L) :-
  repete(N1, C, T).
 
 % 1.Defina um predicado zeroInit(L) que é verdadeiro se L for uma lista que inicia com o número 0 (zero).
-
 zeroInit(L) :- L = [0|_].
-% 2.Defina um predicado has5(L) que é verdadeiro se L for uma lista de 5 elementos. Resolva este exercício sem usar um predicado auxiliar.
 
+% 2.Defina um predicado has5(L) que é verdadeiro se L for uma lista de 5 elementos. Resolva este exercício sem usar um predicado auxiliar.
 has5(L) :- L = [_,_,_,_,_].
 
 % 3.Defina um predicado hasN(L,N) que é verdadeiro se L for uma lista de N elementos.
-
 hasN(L,N) :- length(L,N).
 
 % 4.Defina um predicado potN0(N,L), de forma que L seja uma lista de potências de 2, com expoentes de N a 0.
-
 potN0(-1,[]).
 potN0(N,L) :- 
     L = [A|T],
